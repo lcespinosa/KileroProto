ab15c5280964ad03459ad75a739bf0b2827a1a55
swordfest <alexanderhavarro@gmail.com>
2021-06-20 23:45:09 -0700

    alineacion de 7 jugadores y otros cambios

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,81 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class BallImpulse : MonoBehaviour
+public class BallController : MonoBehaviour
 {
     public float power = 1.0f;
+    public float powerModificator = 1.0f;
     public Rigidbody rb;
     public Vector3 initialTouchPosition;
     public Vector3 finalTouchPosition;
     public float initialTime = 0.0f;
     public float finalTime = 0.0f;
+    public float duration;
+    public float distance;
+    public float rbVelocity = 0.0f;
+
+    //public LineRenderer lineR;
     
+
     // Start is called before the first frame update
     void Start()
     {
+        /*lineR = GetComponent<LineRenderer>();
+        lineR.startWidth = 0.2f;
+        lineR.endWidth = 0.1f;
+        Vector3[] positions = new Vector3[2]
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,0,0)
+            
+            //initialTouchPosition,
+            //finalTouchPosition
+        };
+        lineR.positionCount = 2;*/
         rb = GetComponent<Rigidbody>();
         //rb.AddForce(0,0, thrust, ForceMode.Impulse);
     }
 
-    /*public void OnEndDrag(PointerEventData eventData)
-    {
-        Vector3 dragVectorDirection = (eventData.position - eventData.pressPosition).normalized;
-        GetDragDirection(dragVectorDirection);
-    }
-    
-    private enum DraggedDirection
-    {
-        Up,
-        Down,
-        Right,
-        Left
-    }
-    private DraggedDirection GetDragDirection(Vector3 dragVector)
-    {
-        float positiveX = Mathf.Abs(dragVector.x);
-        float positiveY = Mathf.Abs(dragVector.y);
-        DraggedDirection draggedDir;
-        if (positiveX > positiveY)
-        {
-            draggedDir = (dragVector.x > 0) ? DraggedDirection.Right : DraggedDirection.Left;
-        }
-        else
-        {
-            draggedDir = (dragVector.y > 0) ? DraggedDirection.Up : DraggedDirection.Down;
-        }
-        Debug.Log(draggedDir);
-        return draggedDir;
-    }*/
-    
-    
     // Update is called once per frame
     void Update()
     {
         for (int i = 0; i < Input.touchCount; i++)
         {
+            rbVelocity = rb.velocity.z;
             Touch touch = Input.GetTouch(i);
             
             if (touch.phase == TouchPhase.Began)
             {
                 initialTouchPosition = Camera.main.ScreenToWorldPoint(touch.position);
                 initialTime = Time.time;
+                //lineR.SetPosition(0, initialTouchPosition);
             }
 
             if (touch.phase == TouchPhase.Ended && touch.tapCount == 1)
             {
                 finalTouchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                //lineR.SetPosition(1, finalTouchPosition);
                 finalTime = Time.time;
                 
                 
                 Vector3 direction = finalTouchPosition - initialTouchPosition;
-                float duration = finalTime - initialTime;
-                float distance = direction.magnitude;
+                duration = finalTime - initialTime;
+                distance = direction.magnitude;
                 
-                power = (distance / duration)*2;
+                power = (distance / duration) * powerModificator;
+                Vector3 finalDirection = new Vector3(direction.x * power, direction.y * power, direction.z * power);
                 
                 //rb.AddForceAtPosition(new Vector3(0,0,power),direction,ForceMode.Impulse);
-                rb.AddForce(direction, ForceMode.Impulse);
-                //rb.AddRelativeForce(direction, ForceMode.Impulse);                
+                rb.AddForce(finalDirection, ForceMode.Impulse);
+                //rb.AddRelativeForce(direction, ForceMode.Impulse);            
+                
+                direction = Vector3.zero;
+                duration = 0.0f;
             }
         }
     }
---
+++ Assets/Scripts/TestingLine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingLine : MonoBehaviour
+{
+    public LineRenderer line;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Material mat = new Material(Shader.Find("Unlit/Color"));
+        //mat.color = Color.yellow;
+        //line.materials[0] = mat;
+        line = GetComponent<LineRenderer>();
+        line.startWidth = 0.2f;
+        line.endWidth = 0.2f;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+            line.positionCount = 2;
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                line.SetPosition(0, Camera.main.ScreenToWorldPoint(touch.position));
+            }
+
+            if (touch.phase == TouchPhase.Moved && touch.tapCount == 1)
+            {
+                line.SetPosition(1, Camera.main.ScreenToWorldPoint(touch.position));
+            }
+        }
+    }
+}
